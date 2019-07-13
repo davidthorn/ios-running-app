87677b7b083aa2f10c4072e45523d697b9b0a013
David Thorn <david.thorn221278@googlemail.com>
2019-07-13 14:10:31 +0200

    Initial Commit

---
+++ MyRunningApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyRunningApp
+//
+//  Created by David Thorn on 13.07.19.
+//  Copyright © 2019 David Thorn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
